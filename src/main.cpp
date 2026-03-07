8f616db4e3d2b886aa4ac7162054c5b410e4e298
Galm007 <jmarlpineda21@gmail.com>
2026-03-07 09:05:27 -0500

    Up To 4 IR Sensors Can Be Used Now

--- src/main.cpp
+++ src/main.cpp
@@ -11,35 +11,43 @@ extern "C" {
 #include "vl53l0x.h"  // IR Sensor Library
 }
 
-const gpio_num_t HBridgeEnablePin = GPIO_NUM_2;
+// H-Bridge
 const gpio_num_t HBridgePin1 = GPIO_NUM_26; // Right Wheel Forward
 const gpio_num_t HBridgePin2 = GPIO_NUM_25; // Right Wheel Backward
 const gpio_num_t HBridgePin3 = GPIO_NUM_33; // Left Wheel Backward
 const gpio_num_t HBridgePin4 = GPIO_NUM_32; // Left Wheel Forward
 
+// Motor Encoders
 const gpio_num_t EncoderPin1 = GPIO_NUM_35;
 const gpio_num_t EncoderPin2 = GPIO_NUM_34;
 const gpio_num_t EncoderPin3 = GPIO_NUM_39;
 const gpio_num_t EncoderPin4 = GPIO_NUM_36;
 
+// IR Sensors
 const gpio_num_t I2C_Pin = GPIO_NUM_0;
 const gpio_num_t I2C_SDA_Pin = GPIO_NUM_21;
 const gpio_num_t I2C_SCL_Pin = GPIO_NUM_22;
-const gpio_num_t XShutPin1 = GPIO_NUM_14;
-const gpio_num_t XShutPin2 = GPIO_NUM_12;
+const gpio_num_t XShutPin1 = GPIO_NUM_14; // Right Forward Sensor
+const gpio_num_t XShutPin2 = GPIO_NUM_13; // Left Forward Sensor
+const gpio_num_t XShutPin3 = GPIO_NUM_27;
+const gpio_num_t XShutPin4 = GPIO_NUM_4;
 
 pcnt_unit_handle_t pcnt_unit = NULL;
 BNO08x gyro;
 vl53l0x_t* sensor_right;
 vl53l0x_t* sensor_left;
 
+// Encoders use magnets that rapidly go high and low.
+// These pulses are counted and converted later to radians.
 void init_encoder() {
+	// Initialize pulse counter unit
 	pcnt_unit_config_t unit_config = {
 		.low_limit = -10000,
 		.high_limit = 10000,
 	};
 	pcnt_new_unit(&unit_config, &pcnt_unit);
 
+	// Initialize pulse counter channel
 	pcnt_chan_config_t chan_config = {
 		.edge_gpio_num = EncoderPin1,
 		.level_gpio_num = EncoderPin2,
@@ -47,18 +55,33 @@ void init_encoder() {
 	pcnt_channel_handle_t pcnt_chan = NULL;
 	pcnt_new_channel(pcnt_unit, &chan_config, &pcnt_chan);
 
-	pcnt_channel_set_edge_action(pcnt_chan, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE);
-	pcnt_channel_set_level_action(pcnt_chan, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE);
+	// Describe what a pulse looks like
+	pcnt_channel_set_edge_action(
+		pcnt_chan,
+		PCNT_CHANNEL_EDGE_ACTION_INCREASE,
+		PCNT_CHANNEL_EDGE_ACTION_DECREASE
+	);
+	pcnt_channel_set_level_action(
+		pcnt_chan,
+		PCNT_CHANNEL_LEVEL_ACTION_KEEP,
+		PCNT_CHANNEL_LEVEL_ACTION_INVERSE
+	);
 
+	// Start pulse counter
 	pcnt_unit_enable(pcnt_unit);
 	pcnt_unit_clear_count(pcnt_unit);
 	pcnt_unit_start(pcnt_unit);
 }
 
+#define WAIT_A_BIT() vTaskDelay(pdMS_TO_TICKS(30))
 void init_ir_sensors() {
+	// Turn all sensors off.
+	// When initializing, they are turned on one at a time.
 	gpio_set_level(XShutPin1, 0);
 	gpio_set_level(XShutPin2, 0);
+	WAIT_A_BIT();
 
+	// Set up bus
 	i2c_master_bus_config_t bus_config = {
 		.i2c_port = I2C_Pin,
 		.sda_io_num = I2C_SDA_Pin,
@@ -66,10 +89,9 @@ void init_ir_sensors() {
 		.clk_source = I2C_CLK_SRC_DEFAULT,
 		.glitch_ignore_cnt = 7,
 		.flags = {
-			.enable_internal_pullup = true,
+			.enable_internal_pullup = false,
 		},
 	};
-
 	i2c_master_bus_handle_t bus_handle;
 	if (i2c_new_master_bus(&bus_config, &bus_handle) != ESP_OK) {
 		ESP_LOGE("vl53l0x", "Failed to create bus handle!");
@@ -77,28 +99,33 @@ void init_ir_sensors() {
 
 	// Set up right IR sensor
 	gpio_set_level(XShutPin1, 1);
+	WAIT_A_BIT();
 	sensor_right = vl53l0x_config_with_bus(bus_handle, XShutPin1, 0x29, 1);
 	if (!sensor_right) {
 		ESP_LOGE("vl53l0x", "Failed to initialize Right IR Sensor!");
 	}
 	vl53l0x_init(sensor_right);
 	vl53l0x_setAddress(sensor_right, 0x30);
-	vl53l0x_startContinuous(sensor_right, 0);
+	WAIT_A_BIT();
 
 	// Set up left IR sensor
 	gpio_set_level(XShutPin2, 1);
+	WAIT_A_BIT();
 	sensor_left = vl53l0x_config_with_bus(bus_handle, XShutPin2, 0x29, 1);
 	if (!sensor_left) {
 		ESP_LOGE("vl53l0x", "Failed to initialize Left IR Sensor!");
 	}
 	vl53l0x_init(sensor_left);
-	vl53l0x_setAddress(sensor_left, 0x31);
+	vl53l0x_setAddress(sensor_left, 0x32);
+	WAIT_A_BIT();
+
+	// Start sensors
+	vl53l0x_startContinuous(sensor_right, 0);
 	vl53l0x_startContinuous(sensor_left, 0);
 }
 
 extern "C" void app_main() {
 	// Set pin direction, same as Arduino's pinMode() function
-	gpio_set_direction(HBridgeEnablePin, GPIO_MODE_OUTPUT);
 	gpio_set_direction(HBridgePin1, GPIO_MODE_OUTPUT);
 	gpio_set_direction(HBridgePin2, GPIO_MODE_OUTPUT);
 	gpio_set_direction(HBridgePin3, GPIO_MODE_OUTPUT);
@@ -122,7 +149,6 @@ extern "C" void app_main() {
 	ESP_LOGI("vl53l0x", "Initialized IR Sensors!");
 
 	// Set pin level, same as Arduino's digitalWrite() function
-	gpio_set_level(HBridgeEnablePin, 1);
 	gpio_set_level(HBridgePin1, 1);
 	gpio_set_level(HBridgePin2, 0);
 	gpio_set_level(HBridgePin3, 0);
@@ -146,7 +172,7 @@ extern "C" void app_main() {
 		// Read IR Sensors
 		uint16_t range_right = vl53l0x_readRangeContinuousMillimeters(sensor_right);
 		uint16_t range_left = vl53l0x_readRangeContinuousMillimeters(sensor_left);
-		ESP_LOGI("VL53L0X", "Right: %d mm, Left: %d mm", range_right, range_left);
+		ESP_LOGI("vl53l0x", "Right: %d mm, Left: %d mm", range_right, range_left);
 
 		// Same as arduino's delay() function
 		vTaskDelay(pdMS_TO_TICKS(100));
